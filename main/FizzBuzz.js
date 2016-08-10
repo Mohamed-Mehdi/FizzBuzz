924302e541450e0771da9e23cb4e10091c192f9d
Mohamed Mehdi SAAD <mohamed-mehdi.saad@viseo.com>
2016-08-10 16:51:11 +0200

    first version with tests

--- main/FizzBuzz.js
+++
@@ -1,5 +0,0 @@
-var FizzBuzz = function () {
-
-};
-
-module.exports = FizzBuzz;
--- spec/FizzBuzz.spec.js
+++ spec/FizzBuzz.spec.js
@@ -1,25 +1,25 @@
-var FizzBuzz = require('../main/FizzBuzz');
+var FizzBuzz = require('../lib/FizzBuzz');
 
 describe("FizzBuzz Test", function () {
     it("should have 100 elements", function () {
-        expect(FizzBuzz.length).toBe(100);
+        expect(FizzBuzz().length).toBe(100);
     });
     it("should have 1 as first element", function () {
-        expect(FizzBuzz[0]).toBe(1);
+        expect(FizzBuzz()[0]).toBe(1);
     });
     it("should have 100 as last element", function () {
-        expect(FizzBuzz[FizzBuzz.length - 1]).toBe(100);
+        expect(FizzBuzz()[FizzBuzz().length - 1]).toBe("Buzz");
     });
 
     it("should return Fizz when the number is multiple of 3", function () {
         for (var i = 3; i < 100; i += 3) {
-            expect(FizzBuzz[i]).toBe("Fizz");
+            expect(FizzBuzz()[i - 1]).toBe("Fizz");
         }
     });
     it("should return Fizz when the number is multiple of 3", function () {
         for (var i = 5; i < 100; i += 5) {
             if (i % 3 != 0) {
-                expect(FizzBuzz[i]).toBe("Buzz");
+                expect(FizzBuzz()[i - 1]).toBe("Buzz");
             }
         }
     });
