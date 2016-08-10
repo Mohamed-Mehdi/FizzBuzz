9bc597fae86100b88c3cd681add0a0aa8577805e
Mohamed Mehdi SAAD <mohamed-mehdi.saad@viseo.com>
2016-08-10 16:59:55 +0200

    finishing the second version

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 This application loop numbers from 1 to 100 and if the number is multiple of 3 it print "Fizz" instead of the number and if it's a multiple of 5 it print "Buzz".
 
+For the first version (branch version1):
+if the number is both multiple of 3 and 5 then the privilege is for Fizz
+
+For the second version (branch master)
 If the number is both multiple of 3 and multiple of 5 it print "FizzBuzz"
--- spec/FizzBuzz.spec.js
+++ spec/FizzBuzz.spec.js
@@ -1,5 +1,9 @@
 var FizzBuzz = require('../lib/FizzBuzz');
 
+// Knowing that our prog starts from 1 and not from 0, when the made the loop
+// we should check the value of (i-1), cause our function is a tab starting from 1 and the
+//index of 1 is 0
+
 describe("FizzBuzz Test", function () {
     it("should have 100 elements", function () {
         expect(FizzBuzz().length).toBe(100);
@@ -7,16 +11,26 @@ describe("FizzBuzz Test", function () {
     it("should have 1 as first element", function () {
         expect(FizzBuzz()[0]).toBe(1);
     });
-    it("should have 100 as last element", function () {
+    it("should have Buzz as last element", function () {
         expect(FizzBuzz()[FizzBuzz().length - 1]).toBe("Buzz");
+    });
+
+    it("should return FizzBuzz when the number is multiple of 3 and 5", function () {
+        for (var i = 15; i < 100; i += 5) {
+            if (i % 15 === 0) {
+                expect(FizzBuzz()[i - 1]).toBe("FizzBuzz");
+            }
+        }
     });
 
     it("should return Fizz when the number is multiple of 3", function () {
         for (var i = 3; i < 100; i += 3) {
-            expect(FizzBuzz()[i - 1]).toBe("Fizz");
+            if (i % 5 != 0) {
+                expect(FizzBuzz()[i - 1]).toBe("Fizz");
+            }
         }
     });
-    it("should return Fizz when the number is multiple of 3", function () {
+    it("should return Fizz when the number is multiple of 5", function () {
         for (var i = 5; i < 100; i += 5) {
             if (i % 3 != 0) {
                 expect(FizzBuzz()[i - 1]).toBe("Buzz");
