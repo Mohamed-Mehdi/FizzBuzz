240fda2ead08b83fffd32991581941caadf48582
Mohamed Mehdi SAAD <mohamed-mehdi.saad@viseo.com>
2016-08-10 17:00:59 +0200

    fixing some notation

--- spec/FizzBuzz.spec.js
+++ spec/FizzBuzz.spec.js
@@ -7,7 +7,7 @@ describe("FizzBuzz Test", function () {
     it("should have 1 as first element", function () {
         expect(FizzBuzz()[0]).toBe(1);
     });
-    it("should have 100 as last element", function () {
+    it("should have Buzz as last element", function () {
         expect(FizzBuzz()[FizzBuzz().length - 1]).toBe("Buzz");
     });
 
@@ -16,7 +16,7 @@ describe("FizzBuzz Test", function () {
             expect(FizzBuzz()[i - 1]).toBe("Fizz");
         }
     });
-    it("should return Fizz when the number is multiple of 3", function () {
+    it("should return Fizz when the number is multiple of 5", function () {
         for (var i = 5; i < 100; i += 5) {
             if (i % 3 != 0) {
                 expect(FizzBuzz()[i - 1]).toBe("Buzz");
